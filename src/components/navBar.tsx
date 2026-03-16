8ce3f8db5e3985f372a818a2dffc3405340b4556
VRithea <vongrithea126@gmail.com>
2026-03-16 22:40:13 +0700

    feat(nav-bar): add nav bar and navigations

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,11 +1,27 @@
+import Link from "next/link";
+
 const NavBar: React.FC = () => {
   return (
-    <nav className="sticky top-0 z-99 w-full h-15 flex justify-center items-center">
-      <div className="flex-1 h-full bg-primary border-b border-b-border"></div>
+    <nav className="sticky top-0 z-50 w-full bg-primary border-b border-b-border backdrop-blur-md">
+      <div className="max-w-3xl mx-auto flex justify-between items-center p-4 sm:p-8 md:p-0 h-16">
+        <div className="font-bold text-2xl text-text">
+          <Link href="/">
+            VR
+          </Link>
+        </div>
 
-      <div className="w-3xl h-full border-b border-border bg-primary/70 backdrop-blur-md"></div>
-
-      <div className="flex-1 h-full bg-primary border-b border-b-border"></div>
+        <div className="flex space-x-4 sm:space-x-8 text-text font-medium">
+          <Link href="/#about" className="hover:text-accent transition-colors">
+            About Me
+          </Link>
+          <Link href="/#skills" className="hover:text-accent transition-colors">
+            Skills
+          </Link>
+          <Link href="/projects" className="hover:text-accent transition-colors">
+            Projects
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
